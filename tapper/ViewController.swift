8d127e93ff6c5ca4a99d1c50f47d5ac338ad28ea
Mindframe Mac <Mindframe@mindframes-imac-3.local>
2016-05-23 15:52:25 -0400

    Initial Commit

---
+++ tapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper
+//
+//  Created by Mindframe Mac on 5/23/16.
+//  Copyright © 2016 Mindframe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
